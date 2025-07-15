d3648808e3587c7aecee942a1bfb2829ab51519a
Shubham Kumar <shubhamkr17074@gmail.com>
2025-07-15 21:46:12 +0530

    minor changes

--- vectorstore.py
+++ vectorstore.py
@@ -1,6 +1,6 @@
 import logging
 from langchain_openai import OpenAIEmbeddings
-from langchain.vectorstores import Chroma
+from langchain_community.vectorstores import Chroma
 
 logger = logging.getLogger(__name__)
 
